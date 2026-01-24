2ea4ef26ce1344b9aad2cee039c47a0b9d0a6439
Abdullah <abdullahkamran192@gmail.com>
2026-01-24 13:56:47 +0000

    first commit.

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+const PORT = 8080
+
+app.get("/", (req, res) => {
+    res.send('Hello')
+})
+
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
