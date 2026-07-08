6e4170a2ed3ef8c6941d6e8ee68f416405278f53
Abdullah <abdullahkamran192@gmail.com>
2026-07-08 11:49:21 +0100

    removed the res render login and replaced it with a json message.

--- backend/src/Middlewares/user.js
+++ backend/src/Middlewares/user.js
@@ -1,3 +1,9 @@
 export function isLoggedIn(req, res, next) {
-    req.isAuthenticated() ? next() : res.render("login")
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    return res.status(401).json({
+        message: "Not authenticated"
+    });
 }
